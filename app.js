03b88c1dbd10fb2bd07db68db59cf9e9c14e1c5a
Marlie Martinez <marlieahue@gmail.com>
2024-05-24 12:13:50 -0400

    part 2: creating menu bar & part 3: adding menu buttons

--- app.js
+++ app.js
@@ -10,3 +10,20 @@ const newHeader = document.createElement("h1");
 newHeader.innerText = "DOM Manipulation";
 newHeader.className = "flex-ctr";
 mainEl.append(newHeader);
+
+const topMenuEl = document.getElementById("top-menu");
+topMenuEl.className = "flex-around";
+
+// console.log(mainEl)
+
+function addFunctionality(arr) {
+    arr.flatMap(obj => {
+        let functionality = document.createElement("a")
+        functionality.href = obj.href
+        functionality.textContent = obj.text
+        topMenuEl.append(functionality)
+    });
+
+}
+
+console.log(addFunctionality(menuLinks))
