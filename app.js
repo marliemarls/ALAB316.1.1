688d60ec84ee0dd213a91d3c47b658c83a7491a6
Marlie Martinez <marlieahue@gmail.com>
2024-05-24 11:59:38 -0400

    first part: getting started

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
+var menuLinks = [
+  { text: "about", href: "/about" },
+  { text: "catalog", href: "/catalog" },
+  { text: "orders", href: "/orders" },
+  { text: "account", href: "/account" },
+];
 
+const mainEl = document.querySelector("main");
+const newHeader = document.createElement("h1");
+newHeader.innerText = "DOM Manipulation";
+newHeader.className = "flex-ctr";
+mainEl.append(newHeader);
