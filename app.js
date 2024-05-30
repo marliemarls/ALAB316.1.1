9cbaaa3683078e90e7f3547d19113a97d9edc62c
Marlie Martinez <marlieahue@gmail.com>
2024-05-30 15:59:13 -0400

    i gave up and followed your video :(

--- app.js
+++ app.js
@@ -32,12 +32,12 @@ var menuLinks = [
 const mainEl = document.querySelector("main");
 const newHeader = document.createElement("h1");
 newHeader.innerText = "DOM Manipulation";
-mainEl.className = "flex-ctr";
+mainEl.classList.add("flex-ctr");
 mainEl.append(newHeader);
 
 //part 2: creating menu bar (dom manipulation part2)
 const topMenuEl = document.getElementById("top-menu");
-topMenuEl.className = "flex-around";
+topMenuEl.classList.add("flex-around");
 
 //creating a function that will take in an array
 //the array will be iterated through to grab each obj within the element
@@ -54,12 +54,19 @@ function addMenuButtons(arr) {
 }
 addMenuButtons(menuLinks);
 
+// menuLinks.forEach((obj)=> {
+//   let buttons = document.createElement("a");
+//     buttons.href = obj.href;
+//     buttons.textContent = obj.text;
+//     topMenuEl.append(buttons);
+// })
+
 //part 3: creating sub menu (dom manipulation part 2)
 
 const subMenuEl = document.getElementById("sub-menu");
 subMenuEl.style.height = "100%";
 subMenuEl.style.backgroundColor = "var(--sub-menu-bg)";
-subMenuEl.classList = "flex-around";
+subMenuEl.classList.add("flex-around");
 subMenuEl.style.position = "absolute";
 subMenuEl.style.top = "0";
 
@@ -68,45 +75,47 @@ subMenuEl.style.top = "0";
 //now that topMenuLinks is declared, i can set an event listener
 //event listener will prevent default action that occurs when user clicks mouse
 //also will flatMap the menuLinks in a newly declared variable so that i can use it to check for subLinks and in turn use the subLinks to create a conditional that will make the subMenuEl top property 100% visible
-// console.log(menuLinks)
+
 let topMenuLinks = topMenuEl.querySelectorAll("a");
-// const flatLinks = menuLinks.flatMap((obj, index) => obj.subLinks !== undefined ? obj : menuLinks.splice(index));
-// console.log(menuLinks)
-// console.log(subMenuEl)
 
 topMenuEl.addEventListener("click", (event) => {
   event.preventDefault();
 
   if (event.target.localName !== "a") return;
+  const linkText = event.target.textContent;
+  const linkObject = menuLinks.find((link) => link.text === linkText);
 
-  event.target.className === "" ? (event.target.className = "active") : event.target.classList.remove("active");
-  
-  // flatLinks.forEach((obj) => {
-  //     if (event.target.text === obj.text && obj.hasOwnProperty('subLinks')) {
-  //       subMenuEl.style.top = "100%"
-  //     } else if (event.target.text === obj.text) subMenuEl.style.top = "0";
-  // });
-
-  const linkText = event.target.textContent
-  const linkObject = menuLinks.find((link) => link.text === linkText)
-  console.log(linkObject)
-  if (event.target.text === linkText && Object.keys("subLinks")) { 
-    subMenuEl.style.top = "100%"
-  } else subMenuEl.style.top = "0";
-  
-    // function buildSubMenu(arr){
-    //   subMenuEl.innerHTML = "";
-    //   arr.forEach((obj)=>{
-    //     const subs = obj.subLinks
-    //     const newLinks = document.createElement("a");
-    //     console.log(subs)
-    //     newLinks.href = obj.href;
-    //     newLinks.textContent = obj.text;
-    //     subMenuEl.append(newLinks)
-    //   })
-    // }
-    // buildSubMenu(flatLinks)
-  
+  if (event.target.classList.contains("active")) {
+    event.target.classList.remove("active");
+    subMenuEl.style.top = "0";
+  } else {
+    document
+      .querySelectorAll("top-menu a")
+      .forEach((a) => a.classList.remove("active"));
+    event.target.classList.add("active");
+    if (linkObject.subLinks) {
+      subMenuEl.style.top = "100%";
+      buildSubMenu(linkObject.subLinks);
+    } else {
+      subMenuEl.style.top = "0";
+    }
+  }
 });
 
 //part 5: adding submenu interaction
+function buildSubMenu(sublinks) {
+  subMenuEl.innerHTML = "";
+
+  sublinks.forEach((link) => {
+    const anElem = document.createElement("a");
+    anElem.href = link.href;
+    anElem.textContent = link.text;
+    subMenuEl.append(anElem);
+  });
+}
+
+subMenuEl.addEventListener("click", (event) => {
+  event.preventDefault()
+  if(event.target.localName !== "a") return;
+  console.log("submenu clicked", event.target.textContent)
+})
